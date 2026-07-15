3940577e5ae666895259a0cb6d4cc2d6c6f4184f
Ramon Morillo Ullola <proframonmorillo@gmail.com>
2026-07-15 10:06:34 -0400

    Configuración inicial del proyecto con Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,31 @@
 <?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InicioController;
-use App\Http\Controllers\ContactoController;
-use App\Http\Controllers\ConversionController;
-use App\Http\Controllers\NosotrosController;
-use App\Http\Controllers\ProductosController;
 
-Route::get('/', [InicioController::class, 'index'])->name('inicio');
-Route::get('/contacto', [ContactoController::class, 'index'])->name('contacto');
-Route::get('/conversion', [ConversionController::class, 'index'])->name('conversion');
-Route::get('/nosotros', [NosotrosController::class, 'index'])->name('nosotros');
-Route::get('/productos', [ProductosController::class, 'index'])->name('productos');
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
